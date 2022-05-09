ef298f3626ab2fdbdbb19933a2eefff8c2569b9f
sahil-903 <69524898+sahil-903@users.noreply.github.com>
2022-05-09 11:44:29 +0530

    Update Implement_strStr().cpp

--- 09-05-2022/Implement_strStr().cpp
+++ 09-05-2022/Implement_strStr().cpp
@@ -7,3 +7,17 @@ public:
         return -1;
     }
 };
+
+
+
+/// second method more simplified
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int found = haystack.find(needle);
+        if (found != -1)
+            return found;
+        return -1;
+    }
+};
