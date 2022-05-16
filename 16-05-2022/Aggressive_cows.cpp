06f6a0b39e0dda1694bffb44ae2dabbf1d7220f8
sahil-903 <69524898+sahil-903@users.noreply.github.com>
2022-05-16 15:06:36 +0530

    Update Aggressive_cows.cpp

--- 16-05-2022/Aggressive_cows.cpp
+++ 16-05-2022/Aggressive_cows.cpp
@@ -1,33 +1,36 @@
 // (SPOJ question)
 #include <bits/stdc++.h>
 
-using namespace std;
-bool isCompatible(vector < int > inp, int dist, int cows) {
-  int n = inp.size();
-  int k = inp[0];
-  cows--;
-  for (int i = 1; i < n; i++) {
-    if (inp[i] - k >= dist) {
-      cows--;
-      if (!cows) {
-        return true;
+    using namespace std;
+    bool isPossible(int a[], int n, int cows, int minDist) {
+      int cntCows = 1;
+      int lastPlacedCow = a[0];
+      for (int i = 1; i < n; i++) {
+        if (a[i] - lastPlacedCow >= minDist) {
+          cntCows++;
+          lastPlacedCow = a[i];
+        }
       }
-      k = inp[i];
+      if (cntCows >= cows) return true;
+      return false;
     }
-  }
-  return false;
-}
-int main() {
-  int n = 5, m = 3;
-  vector<int> inp {1,2,8,4,9};
-  sort(inp.begin(), inp.end());
-  int maxD = inp[n - 1] - inp[0];
-  int ans = INT_MIN;
-  for (int d = 1; d <= maxD; d++) {
-    bool possible = isCompatible(inp, d, m);
-    if (possible) {
-      ans = max(ans, d);
+    int main() {
+      int n = 5, cows = 3;
+      int a[]={1,2,8,4,9};
+      sort(a, a + n);
+
+      int low = 1, high = a[n - 1] - a[0];
+
+      while (low <= high) {
+        int mid = (low + high) >> 1;
+
+        if (isPossible(a, n, cows, mid)) {
+          low = mid + 1;
+        } else {
+          high = mid - 1;
+        }
+      }
+      cout << "The largest minimum distance is " << high << endl;
+
+      return 0;
     }
-  }
-  cout << "The largest minimum distance is " << ans << '\n';
-}
